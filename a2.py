90925d96dafb50b253aebc8c6ed20daed39b72cd
dilara2016 <m.123.hunain@gmail.com>
2025-05-18 12:47:48 +0200

    a2

---
+++ a2.py
@@ -0,0 +1,15 @@
+def NW(s):
+    if (s ==" "):
+        return "a"
+    i = len(s) - 1
+    while(s[i] =='z' and i >= 0):
+        i -= 1
+        if (i ==-1):
+            s = s+'a'
+        else:
+            s = s.replace(s[i], chr(ord(s[i])+1), +1)
+        return s
+    
+inp = input("Enter Input: ")
+print(NW(inp))
+
